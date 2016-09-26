de8601db324adf7ff28659773b0bccec63846b1b
alray2569 <alr2569@gmail.com>
2016-09-25 20:10:38 -0400

    Graphical User Interface work Rename "cube" to "MenuMain" Add "MenuInventory" and "MenuImgGallery" Add navigation script (navigation.cs)

---
+++ Assets/navigation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class navigation : MonoBehaviour {
+
+	public void LoadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
