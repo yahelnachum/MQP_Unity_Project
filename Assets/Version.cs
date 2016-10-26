5ad68aba6cf10671b3b9975117f203960e175c97
Andrew Ray <alr2569@gmail.com>
2016-10-26 16:18:37 -0400

    Add version number to main menu

---
+++ Assets/Version.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Version : MonoBehaviour {
+
+    // <branch> YYMMDD.hhmm
+    public const string VersionString = "HTTPCam 161026.1611";
+
+	// Use this for initialization
+	void Start () {
+        GameObject tVersionNo = GameObject.Find("tVersionNo");
+        tVersionNo.GetComponent<Text>().text = VersionString;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
