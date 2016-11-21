87512633899f784e181e000b2438f9eecf15ecd1
Yahel Nachum <yahelnachum@yahoo.com>
2016-11-21 00:20:44 -0500

    Integrated splash screen

---
+++ Assets/SplashScreen.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreen : MonoBehaviour {
+
+	private float waitTime = 1f; // in seconds
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (SwitchToMainMenu ());
+	}
+
+	IEnumerator SwitchToMainMenu(){
+		Debug.Log ("splash screen \"loading\"");
+		yield return new WaitForSeconds (waitTime);
+		Debug.Log ("splash screen \"loaded\"");
+		SwitchPanels.changePanelStatic ("pSplash:deactivate,pMain:activate");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/SwitchPanels.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchPanels : MonoBehaviour {
+
+	const string ACTIVATE = "activate";
+	const string DE_ACTIVATE = "deactivate";
+
+	public void changePanel(string str){
+		changePanelStatic (str);
+	}
+
+	public static void changePanelStatic(string str){
+
+		if (PlayerData.getInstance ().getCurrentNarrativeChunk() == 0) {
+
+			char[] panelSplits = { ',' };
+			char[] instructionsSplits = { ':' };
+
+			string[] panels = str.Split (panelSplits);
+
+			for (int i = 0; i < panels.Length; i++) {
+				string[] panelInstruction = panels [i].Split (instructionsSplits);
+				Debug.Log ("tutorial obj name: " + panelInstruction [0]);
+				GameObject panel = StartGame.findInactive (panelInstruction [0], "vMenu");
+
+				if (panelInstruction [1].CompareTo (ACTIVATE) == 0) {
+					Debug.Log ("activate");
+					panel.SetActive (true);
+				} else {
+					Debug.Log ("deactivate");
+					panel.SetActive (false);
+				}
+			}
+		}
+	}
+}
