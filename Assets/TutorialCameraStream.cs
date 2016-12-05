ca496337c88dec706bcc334dafec27f14b3a01e0
Yahel Nachum <yahelnachum@yahoo.com>
2016-12-04 22:52:12 -0500

    Finished Tutorial pop ups in unity.

---
+++ Assets/TutorialCameraStream.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialCameraStream : MonoBehaviour {
+
+	private float gopherIntroductionWaitTime = 1f; // in seconds
+	private float gopherPinchAndZoomExplainationWaitTime = 3f; // in seconds
+	private float gopherAnalyzeButtonExplainationWaitTime = 3f; // in seconds
+
+	void Start(){
+		StartCoroutine (StartTutorialTimer ());
+	}
+
+	private IEnumerator StartTutorialTimer(){
+		if (PlayerData.getInstance ().getCurrentNarrativeChunk () == 0) {
+			yield return new WaitForSeconds (gopherIntroductionWaitTime);
+			SwitchPanels.changePanelStatic ("pCameraStreamTutorial:activate");
+			yield return new WaitForSeconds (gopherPinchAndZoomExplainationWaitTime);
+			SwitchPanels.changePanelStatic ("pPinchAndZoomTutorial:deactivate,pAnalyzeButtonTutorial:activate");
+			yield return new WaitForSeconds (gopherAnalyzeButtonExplainationWaitTime);
+			SwitchPanels.changePanelStatic ("pCameraStreamTutorial:deactivate");
+		}
+	}
+}
---
+++ Assets/TutorialMainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialMainMenu : MonoBehaviour {
+
+
+	private float gopherIntroductionWaitTime = 1f; // in seconds
+	private float gopherObjectListExplainationWaitTime = 3f; // in seconds
+	private float gopherCameraButtonExplainationWaitTime = 3f; // in seconds
+
+	void Start(){
+		StartCoroutine (StartTutorialTimer ());
+	}
+
+	private IEnumerator StartTutorialTimer(){
+		if (PlayerData.getInstance ().getCurrentNarrativeChunk () == 0) {
+			yield return new WaitForSeconds (gopherIntroductionWaitTime);
+			SwitchPanels.changePanelStatic ("pMainTutorial:activate");
+			yield return new WaitForSeconds (gopherObjectListExplainationWaitTime);
+			SwitchPanels.changePanelStatic ("pObjectListTutorial:deactivate,pCameraButtonTutorial:activate");
+			yield return new WaitForSeconds (gopherCameraButtonExplainationWaitTime);
+			SwitchPanels.changePanelStatic ("pMainTutorial:deactivate");
+		}
+	}
+}
