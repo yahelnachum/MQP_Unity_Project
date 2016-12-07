1a8869d3ac9e0b3d95fb07581e9f62410a833983
Thomas J Meehan <tmeehan@wpi.edu>
2016-12-06 23:20:53 -0500

    Button progress now works fully

--- Assets/Scripts/UpdatePanel.cs
+++ Assets/Scripts/UpdatePanel.cs
@@ -6,24 +6,38 @@ public class UpdatePanel : MonoBehaviour {
 
 		Debug.Log("enter func val is" + val);
 
+		for (int i = 0; i < 100; i++) {
+			
 
-		GameObject bar = GameObject.Find ("pLoadingBar");
+			GameObject bar = GameObject.Find ("pLoadingBar");
 
-		RectTransform rect = bar.GetComponent<RectTransform> ();
+			RectTransform rect = bar.GetComponent<RectTransform> ();
 
-		//float temp = rect.anchorMax.x;
+			float temp = rect.anchorMax.x;
 
-		rect.anchorMax = new Vector2 (val, 1);
-		rect.position = new Vector2(0,0);
+			val = temp + 0.01f;
 
-		//rect.anchorMax = new Vector2 (0.9f, 1);
+			rect.anchorMax = new Vector2 (val, 1);
+			//rect.position = new Vector2 (0, 0);
 
-		Debug.Log("update before panel val is" + val);
+			//rect.anchorMax = new Vector2 (0.9f, 1);
 
-		yield return new WaitForSeconds(1);
+			Debug.Log ("update before panel val is" + val);
 
-		Debug.Log("update after panel val is" + val);
+			yield return new WaitForSeconds (0.02f);
 
+			Debug.Log ("update after panel val is" + val);
+
+		}
+
+	}
+
+
+	public void startUpdate(){
+
+	
+		StartCoroutine (UpdatePanel.progress (0.1f));
+		Debug.Log("update 1");
 
 
 	}
