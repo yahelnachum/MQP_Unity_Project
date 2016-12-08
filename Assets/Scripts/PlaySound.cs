bc5f86fe782022e2a705b01ab3387ad277455945
Thomas J Meehan <tmeehan@wpi.edu>
2016-12-07 20:24:34 -0500

    Added sound on button press functionality, and some narrative scripting for the update panel

--- Assets/Scripts/PlaySound.cs
+++ Assets/Scripts/PlaySound.cs
@@ -3,13 +3,48 @@ using System.Collections;
 
 public class PlaySound : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+
+	private static PlaySound instance = new PlaySound();
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="UpdatePanel"/> class.
+	/// Made private so that it can be a singleton.
+	/// </summary>
+	private PlaySound(){}
+
+	/// <summary>
+	/// Returns the single instance of the UpdatePanel.
+	/// </summary>
+	/// <returns>The single instance of the UpdatePanel class.</returns>
+	public static PlaySound getInstance(){
+		if (instance == null) {
+			GameObject singleton = new GameObject ();
+			instance = singleton.AddComponent<PlaySound> ();
+		}
+		return instance;
+	}
+
+	public void Start(){
+		PlaySound.getInstance();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+
+	//used by button to call sound function
+	public void startSound( string sound){
+
+		//UpdatePanel.getInstance().StartCoroutine(progress (0.1f));
+		//PlaySound.getInstance().StartCoroutine(begin(sound));
+
+		Debug.Log ("NOW PLAYING" + sound);
+
+
+	}
+
+
+
 }
