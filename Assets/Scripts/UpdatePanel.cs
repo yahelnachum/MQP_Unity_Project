8d0f451acabea65dc61f010391a8d668f6e2dd06
Yahel Nachum <ynachum@wpi.edu>
2016-12-10 16:35:01 -0500

    Made update panel animation smoother.

--- Assets/Scripts/UpdatePanel.cs
+++ Assets/Scripts/UpdatePanel.cs
@@ -30,6 +30,19 @@ public class UpdatePanel : MonoBehaviour {
 		UpdatePanel.getInstance();
 	}
 
+	float fps = 60;
+
+	public  IEnumerator load(float start, float end, float seconds, RectTransform rect){
+		for (float current = start; current < end; current += (end - start) / (seconds * fps)) {
+			float init = Time.realtimeSinceStartup;
+			rect.anchorMax = new Vector2 (current, 1);
+
+			float delta = Time.realtimeSinceStartup - init;
+			if (delta < 1f / fps) {
+				yield return new WaitForSeconds (1f / fps - delta);
+			}
+		}
+	}
 
 	/// <summary>
 	/// Increase the progress bar element on the update panel by looping, adjusting anchors, and waiting.
@@ -52,41 +65,19 @@ public class UpdatePanel : MonoBehaviour {
 
 		GameObject icon = GameObject.Find ("iLoading");
 
+		Debug.Log ("loading 8888888888");
 
-		for (int i = 0; i < 100; i++) {
-			
-			float temp = rect.anchorMax.x;
-			val = temp + 0.01f;
+		yield return StartCoroutine(load (0f, 0.2f, 1f, rect));
+		word.text = "Isn't this fun?";
+		yield return StartCoroutine(load (0.2f, 0.4f, 2f, rect));
+		word.text = "Just a bit longer";
+		yield return StartCoroutine(load (0.4f, 0.6f, 1f, rect));
+		word.text = "Don't worry, all your files are exactly where you left them";
+		yield return StartCoroutine(load (0.6f, 0.8f, 4f, rect));
+		word.text = "Almost there!";
+		yield return StartCoroutine(load (0.8f, 1.0f, 1f, rect));
 
-			rect.anchorMax = new Vector2 (val, 1);
-			//rect.position = new Vector2 (0, 0);
-			//rect.anchorMax = new Vector2 (0.9f, 1);
-
-			if (i == 20) {
-				word.text = "Isn't this fun?";
-			}
-
-			if (i == 40) {
-				word.text = "Just a bit longer";
-			}
-
-			if (i == 60) {
-				word.text = "Don't worry, all your files are exactly where you left them";
-			}
-
-			if (i == 80) {
-				word.text = ";)";
-			}
-
-			if (i == 85) {
-				word.text = "Almost there!";
-			}
-
-			spin (icon);
-				
-			yield return new WaitForSeconds (0.3f);
-
-		}
+		//spin (icon);
 
 		word.text = "Done!";
 
@@ -98,6 +89,8 @@ public class UpdatePanel : MonoBehaviour {
 		song.Stop ();
 
 		end (song);
+
+		yield return null;
 
 	}
 
